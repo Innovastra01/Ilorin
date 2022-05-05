29d70817af27f1fdcf203e9e210e5729b1d94235
KelechiOkoroafor <okoroaforkelechi123@gmail.com>
2022-05-05 16:04:00 +0100

    fixing and editing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import Blogs from "./components/Blogs";
+import BuySell from "./components/BuySell";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/ScrollToTop";
+import Designs from "./components/designs";
+import scrollreveal from "scrollreveal";
 
-function App() {
+export default function App() {
+  useEffect(() => {
+    const registerAnimations = () => {
+      const sr = scrollreveal({
+        origin: "bottom",
+        distance: "80px",
+        duration: 2000,
+        reset: false,
+      });
+      sr.reveal(
+          `
+        nav,
+        #home,
+        #buySell1,
+        #buySell2,
+        #buySell3,
+        #blogs,
+        footer
+    `,
+          {
+            opacity: 0,
+            interval: 500,
+          }
+      );
+    };
+    registerAnimations();
+  }, []);
+  window.setTimeout(() => {
+    const home = document.getElementById("home");
+    home.style.transform = "none";
+  }, 1500);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <ScrollToTop />
+        <Navbar />
+        <Home />
+        <Designs />
+        <BuySell />
+        <Blogs />
+        <Footer />
+      </div>
   );
 }
-
-export default App;
